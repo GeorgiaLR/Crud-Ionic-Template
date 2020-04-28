e4cdaf3f82ea71881d34a5252f2eb57e01a1e26d
GeorgiaLR <cordonnier.l@gmail.com>
2020-04-28 12:03:06 +0200

    first draft

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+// Added after npm install firebase angularfire2
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { FIREBASE_CONFIG } from './firebase.credentials';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireDatabaseModule],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyCTXb_t0b7IuElC6eQlWuiWCVI4vPmB8Mg",
+    authDomain: "fir-test-e0574.firebaseapp.com",
+    databaseURL: "https://fir-test-e0574.firebaseio.com",
+    projectId: "fir-test-e0574",
+    storageBucket: "fir-test-e0574.appspot.com",
+    messagingSenderId: "530819796048",
+    appId: "1:530819796048:web:de417ce7d49860fbf49555"
+}
